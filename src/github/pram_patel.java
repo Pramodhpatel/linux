fe9f81388389e8869f8aeb3d24df216507dfe263
qolsysjci <pramodhhnyy@gmail.com>
2023-05-10 23:35:04 +0530

    3rd commit

---
+++ src/github/pram_patel.java
@@ -0,0 +1,10 @@
+package github;
+
+public class pram_patel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("iam gonna learnLll>>");
+	}
+
+}
