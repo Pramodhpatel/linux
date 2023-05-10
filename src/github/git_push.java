47fa42655ecbdd088e30460ca02b6317e25871cc
qolsysjci <qolsysjci@pramodhpatel.lan>
2023-05-10 12:57:44 +0530

    first commit

---
+++ src/github/git_push.java
@@ -0,0 +1,10 @@
+package github;
+
+public class git_push {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author qolsysjci
+ *
+ */
+module Git_practice {
+}
